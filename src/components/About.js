8d96cb85cbc868762ce6b61d7fc99e73fa084f09
manish785 <kumarachilish1997@gmail.com>
2023-12-09 16:36:07 +0530

    fix : refactored the code of About Component

--- src/components/About.js
+++ src/components/About.js
@@ -12,8 +12,7 @@ class About extends React.Component {
     render() {
         return (
             <div>
-                <h1>About</h1>
-                <h2>This is Namaste React Web Series</h2>
+                <h1>About Me</h1>
                 <UserClass name={'first'} location={'Darbhanga Class'}/>
             </div>
         )
