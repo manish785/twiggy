485de84c3f44775ea24d25034df1a3d9d5aa7181
manish785 <kumarachilish1997@gmail.com>
2023-12-26 19:11:35 +0530

    fix : changed the title of the App and wrote test cases for the Header Component

---
+++ src/components/__test__/Header.test.js
@@ -0,0 +1,77 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import appStore from '../../utils/appStore';
+import Header from '../Header';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+
+it("Should render Header Component with a login button", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={appStore}>
+          <Header />
+        </Provider>
+      </BrowserRouter>
+    );
+  
+    const loginButton = screen.getByRole("button", { name : 'login'});
+  
+    //const loginButton = screen.getByText("Login");
+  
+    expect(loginButton).toBeInTheDocument();
+  });
+  
+
+it("Should render Header Component with a Cart Items 0", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={appStore}>
+          <Header />
+        </Provider>
+      </BrowserRouter>
+    );
+  
+    const cartItem = screen.getByText('Cart - (0 items)');
+  
+    expect(cartItem).toBeInTheDocument();
+  });
+ 
+
+  it("Should render Header Component with a Cart item ", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={appStore}>
+          <Header />
+        </Provider>
+      </BrowserRouter>
+    );
+  
+    const cartItem = screen.getByText(/Cart/);
+  
+    expect(cartItem).toBeInTheDocument();
+  });
+ 
+ 
+  it("Should change Login Button to Logout on click", () => {
+    render(
+      <BrowserRouter>
+        <Provider store={appStore}>
+          <Header />
+        </Provider>
+      </BrowserRouter>
+    );
+  
+    const loginButton = screen.getByRole("button", { name: "Login" });
+  
+    fireEvent.click(loginButton);
+  
+    const logoutButton = screen.getByRole("button", { name: "Logout" });
+  
+    expect(logoutButton).toBeInTheDocument();
+  });
+ 
+ 
+
+
+
+
